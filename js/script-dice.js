b218df71d898e5375b51216deef3e36487a69dc2
nicolozibra1 <nico.zibra@gmail.com>
2023-03-14 10:19:31 +0100

    dice-game-add-script

--- js/script-dice.js
+++ js/script-dice.js
@@ -1,8 +1,37 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
 // MYCODE - DICE GAME
 const btnPlay = document.getElementById('playnow');
 console.dir(btnPlay);
 
 btnPlay.addEventListener('click', function() {
-    const chooseNumber = document.getElementById('choose-number').value;
-    console.log(chooseNumber);   
-})
+    let chooseNumber = document.getElementById('choose-number').value;
+    console.log(chooseNumber); 
+    
+    // GENERATE NUMBER
+    let botNumber = getRndNumber(1, 6);
+    console.log(botNumber);
+
+    // RESULT
+    const result = document.querySelector('.box-result');
+    if (chooseNumber > botNumber){
+        result.innerHTML = `<h3>Il tuo numero: ${chooseNumber}</h3>
+                            <h3>Il numero del Bot: ${botNumber}</h3>
+                            <h1>Hai vinto</h1>`;
+    }   
+    else if (chooseNumber == botNumber){
+        result.innerHTML = `<h3>Il tuo numero: ${chooseNumber}</h3>
+                            <h3>Il numero del Bot: ${botNumber}</h3>
+                            <h1>Pareggio</h1>`;
+    }
+    else if (chooseNumber < botNumber){
+        result.innerHTML = `<h3>Il tuo numero: ${chooseNumber}</h3>
+                            <h3>Il numero del Bot: ${botNumber}</h3>
+                            <h1>Hai perso</h1>`;
+    }
+})  
+
+
+
